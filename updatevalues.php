2d6069b12ec414f409d7781b5fb6ece1b5d82cdc
Julian GOnzalez <cocoriotos@hotmail.com>
2024-08-10 21:28:29 -0500

    salaryupdate

--- updatevalues.php
+++ updatevalues.php
@@ -22,13 +22,11 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-print $salarymonthly;
-print $vehicle;
-print $usernamer;
 
 
 ##$query="UPDATE drivers_opsvalues SET  salarymonthly = '$salarymonthly', gasolinekms ='$gasolinekms', gasolinefull = '$gasolinefull', oilfull = '$oilfull', soatyearly = '$soatyearly', technomechanicalyearly ='$technomechanicalyearly', quarterlymaintenance = '$quarterlymaintenance', anuallytaxes = '$anuallytaxes', tollcost = '$tollcost', drivenhourcostcity = '$drivenhourcostcity', drivenhourcostrural = '$drivenhourcostrural', unforeseencity = '$unforeseencity', unforeseenrural = '$unforeseenrural', hoursbyday = '$hoursbyday'  where vehicle = '$vehicle' and user='$usernamer'";
-$query="UPDATE drivers_opsvalues SET  salarymonthly = '$salarymonthly' where vehicle = '$vehicle' and user='$usernamer'";
+$query="UPDATE drivers_opsvalues SET  salarymonthly = '$salarymonthly'; 
+##where vehicle = '$vehicle' and user='$usernamer'";
 $resultado= $conn ->query($query);
 
 if ($resultado){
