4285c4aef00197e6add4f44e156888e1638a8589
Julian GOnzalez <cocoriotos@hotmail.com>
2024-08-11 00:11:15 -0500

    interline2

--- DriversQuotationFinal.php
+++ DriversQuotationFinal.php
@@ -58,11 +58,11 @@
 		<?php //include "header.php";?>
 		<footer id="contact">
 				<hgroup>
-					<h6 align="center"><address><strong>Web Admin:</strong> Julian Gonzalez Bucheli</address></h6>
-					<h6 align="center"><address><strong>Email:</strong>cocoriotos@hotmail.com</address></h6>
-					<h6 align="center"><address><strong>Phone:</strong>(+57) 305 429 31 85 </address></h6>
-					<h6 align="center"><address><strong>Pereira - Colombia </strong></address></h6>
-					<h6 time align="center" datetime="2021/4/1" pubdate>Published on Date: 01/04/2021 - WEB Page available: Monday to Friday  8 a.m. to 5 p.m.</time></h6> <!-- Publication format layout-->
+					<h4 align="center"><address><strong>Web Admin:</strong> Julian Gonzalez Bucheli</address></h4>
+					<h4 align="center"><address><strong>Email:</strong>cocoriotos@hotmail.com</address></h4>
+					<h4 align="center"><address><strong>Phone:</strong>(+57) 305 429 31 85 </address></h4>
+					<h4 align="center"><address><strong>Pereira - Colombia </strong></address></h4>
+					<h4 time align="center" datetime="2021/4/1" pubdate>Published on Date: 01/04/2021 - WEB Page available: Monday to Friday  8 a.m. to 5 p.m.</time></h4> <!-- Publication format layout-->
 				</hgroup>
 		</footer>
 	</body>
