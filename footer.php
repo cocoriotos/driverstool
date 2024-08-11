614d4117118377049edac80c07214b048d6cd386
Julian GOnzalez <cocoriotos@hotmail.com>
2024-08-11 00:36:28 -0500

    footer

--- footer.php
+++ footer.php
@@ -2,23 +2,15 @@
  <html lang="us">
 <link rel="stylesheet" href="tab.css">
  <footer id="contact">
- <!--<div class="rectangle1">-->
+ <div class="rectangle1">
     <hgroup>
-        <!--<h6 ><address><strong>Desarrollado por: Julián González Bucheli </strong></address></h6>
+        <h6 ><address><strong>Desarrollado por: Julián González Bucheli </strong></address></h6>
         <h6 ><address><strong>Correo: </strong><a href="mailto:cocoriotos@hotmail.com " style="color: white;">cocoriotos@hotmail.com</a></address></h6>
         <h6 ><address><strong>Celular: </strong>(+57) 305 4293185 </address></h6>
         <h6 ><address><strong>Pereira - Colombia </strong></address></h6>
         <h6 time datetime="2024/1/4" pubdate>Fecha de Publicación: 04/01/2024 - ´Horario de Soporte para la página WEB : Lunes to Viernes  8 a.m. a 5 p.m.</time></h6> <!-- Publication format layout--> 
-        
-        <h6 align="center"><address><strong>Web Admin:</strong> Julian Gonzalez Bucheli</address></h6>
-		<h6 align="center"><address><strong>Email:</strong>cocoriotos@hotmail.com</address></h6>
-		<h6 align="center"><address><strong>Phone:</strong>(+57) 305 429 31 85 </address></h6>
-		<h6 align="center"><address><strong>Pereira - Colombia </strong></address></h6>
-		<h6 time align="center" datetime="2021/4/1" pubdate>Published on Date: 01/04/2021 - WEB Page available: Monday to Friday  8 a.m. to 5 p.m.</time></h6> <!-- Publication format layout-->
-				   		
-	
 	</hgroup>
-  <!--</div>-->
+  </div>
 </footer>
  
 </html>
